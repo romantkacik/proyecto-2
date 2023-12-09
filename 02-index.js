e75a0edee11128cdfe492ccf2d506c6a68fc7446
romantkacik <123880795+romantkacik@users.noreply.github.com>
2023-12-09 18:31:24 +0100

    cargado de array

---
+++ 02-index.js
@@ -0,0 +1,124 @@
+const products = [
+  {
+    name: 'Apple MacBook Pro Apple M1 Pro/16GB/1TB SSD/16.2" Plata',
+    brand: 'Apple',
+    price: 2354 + '€',
+    stars: 5,
+    reviews: 13,
+    seller: 'Mackfan',
+    image:
+      './assets/assets-proyectp-2/imagenes/APPLE/1145-apple-macbook-pro-apple-m1-pro-16gb-1tb-ssd-162-plata.webp',
+  },
+  {
+    name: 'Apple Macbook Air 2023 Apple M2/8GB/256GB SSD/GPU Deca Core/15.3 Gris Espacial',
+    brand: 'Apple',
+    price: 1440 + '€',
+    stars: 4,
+    reviews: 65,
+    seller: 'PcComponentes',
+    image:
+      './assets/assets-proyectp-2/imagenes/APPLE/1255-apple-macbook-air-2023-apple-m2-8gb-512gb-ssd-gpu-deca-core-153-gris-espacial.webp',
+  },
+  {
+    name: 'Apple Macbook Pro Apple M3 8 Núcleos/8GB/512GB SSD/GPU 10 Núcleos/14" Gris Espacial',
+    brand: 'Apple',
+    price: 1928 + '€',
+    stars: 5,
+    reviews: 1,
+    seller: 'PcComponentes',
+    image:
+      './assets/assets-proyectp-2/imagenes/APPLE/1271-apple-macbook-pro-apple-m3-8-nucleos-8gb-512gb-ssd-gpu-10-nucleos-14-gris-espacial-foto.webp',
+  },
+  {
+    name: 'Apple MacBook Air Apple M1/8GB/256GB SSD/GPU Hepta Core/13.3 Plata',
+    brand: 'Apple',
+    price: 959 + '€',
+    stars: 4,
+    reviews: 718,
+    seller: 'PcComponentes',
+    image:
+      './assets/assets-proyectp-2/imagenes/APPLE/1911-apple-macbook-air-apple-m1-8gb-256gb-ssd-gpu-hepta-core-133-plata.webp',
+  },
+  {
+    name: 'ASUS TUF Gaming A15 2023 FA507NU-LP045 AMD Ryzen 7 7735HS/16GB/512GB SSD/RTX 4050/15.6',
+    brand: 'Asus',
+    price: 999 + '€',
+    stars: 3,
+    reviews: 17,
+    seller: 'PcComponentes',
+    image:
+      './assets/assets-proyectp-2/imagenes/ASUS/1737-asus-tuf-gaming-a15-2023-fa507nu-lp045-amd-ryzen-7-7735hs-16gb-512gb-ssd-rtx-4050-156.webp',
+  },
+  {
+    name: 'ASUS Vivobook Go E1504FA-NJ642W AMD Ryzen 5 7520U/8GB/512GB SSD/15.6',
+    brand: 'Asus',
+    price: 469 + '€',
+    stars: 2,
+    reviews: 33,
+    seller: 'Mackfan',
+    image:
+      './assets/assets-proyectp-2/imagenes/ASUS/1810-asus-vivobook-go-e1504fa-nj642w-amd-ryzen-5-7520u-8gb-512gb-ssd-radeon-610m-156.webp',
+  },
+  {
+    name: 'HP Victus 15-fa0053ns Intel Core i5-12450H/16GB/512GB SSD/RTX 3050/15.6',
+    brand: 'HP',
+    price: 729 + '€',
+    stars: 5,
+    reviews: 23,
+    seller: 'PcComponentes',
+    image:
+      './assets/assets-proyectp-2/imagenes/HP/1616-hp-victus-16-s0011ns-amd-ryzen-7-7840hs-32gb-1tb-ssd-rtx-4060-161.webp',
+  },
+  {
+    name: 'HP ZBook Studio G9 Intel Core i7-12700H/32GB/512GB SSD/RTX 3060/16',
+    brand: 'HP',
+    price: 2820 + '€',
+    stars: 4,
+    reviews: 40,
+    seller: 'Mackfan',
+    image:
+      './assets/assets-proyectp-2/imagenes/HP/1793-hp-zbook-studio-g9-intel-core-i7-12700h-32gb-512gb-ssd-rtx-3060-16.webp',
+  },
+  {
+    name: 'HP 255 G9 AMD Ryzen 5 5625U/16GB/512 GB SSD/15.6',
+    brand: 'HP',
+    price: 499 + '€',
+    stars: 3,
+    reviews: 13,
+    seller: 'Mackfan',
+    image:
+      './assets/assets-proyectp-2/imagenes/HP/1951-hp-470-g10-intel-core-i7-1355u-32gb-1tb-ssd-mx550-173.webp',
+  },
+  {
+    name: 'Lenovo IdeaPad Slim 3 15IAH8 Intel Core i5-12450H/8GB/512GB SSD/15.6',
+    brand: 'Lenovo',
+    price: 399 + '€',
+    stars: 3,
+    reviews: 38,
+    seller: 'Mackfan',
+    image:
+      './assets/assets-proyectp-2/imagenes/LENOVO/1211-lenovo-ideapad-slim-3-15iah8-intel-core-i5-12450h-8gb-512gb-ssd-156.webp',
+  },
+  {
+    name: 'Lenovo IdeaPad Flex 5 14IAU7 Intel Core i7-1255U/16GB/512GB SSD/14" Táctil',
+    brand: 'Lenovo',
+    price: 929 + '€',
+    stars: 2,
+    reviews: 110,
+    seller: 'Mackfan',
+    image:
+      './assets/assets-proyectp-2/imagenes/LENOVO/1293-lenovo-ideapad-flex-5-14iau7-intel-core-i7-1255u-16gb-512gb-ssd-14-tactil.webp',
+  },
+  {
+    name: 'Lenovo ThinkPad L15 Gen 4 Intel Core i7-1355U/16GB/512GB SSD/15.6',
+    brand: 'Lenovo',
+    price: 1526 + '€',
+    stars: 3,
+    reviews: 68,
+    seller: 'PcComponentes',
+    image:
+      './assets/assets-proyectp-2/imagenes/LENOVO/1353-lenovo-thinkpad-l15-gen-4-intel-core-i7-1355u-16gb-512gb-ssd-156.webp',
+  },
+]
+
+console.log(products)
